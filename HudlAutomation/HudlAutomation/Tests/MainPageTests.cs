f2b5e164b8948c44e32f9ae7d27b58ac74009d7d
farqai <fqa@codehousegroup.com>
2018-12-29 16:17:29 +0000

    Commit of framework, API test spreadsheet and POM.

---
+++ HudlAutomation/HudlAutomation/Tests/MainPageTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HudlAutomation.Utilities;
+using NUnit.Framework;
+
+namespace HudlAutomation.Tests
+{
+    [TestFixture]
+    class MainPageTests : BaseSetupTearDown
+    {
+        [Test]
+        public void CanLoginSuccessfully()
+        {
+
+        }
+    }
+}
---
+++ HudlAutomation/HudlAutomation/Utilities/WebDriverConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace HudlAutomation.Utilities
+{
+    class WebDriverConfig
+    {
+        private IWebDriver _driver;
+   
+        public WebDriverConfig(IWebDriver context)
+        {
+            this._driver = context;
+        }
+
+        public IWebDriver InitLocalDriver()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl(ConfigurationManager.AppSettings.Get("url"));
+            return _driver;
+        }
+
+        public void CleanUp()
+        {
+            _driver.Quit();
+        }
+    }
+}
